8ef5a040fc11b748acc8b8ec2c451fef17c82a90
Sung Kil Han <90734610+greatandrewhan@users.noreply.github.com>
2023-09-15 00:00:21 -0400

    webSocket fetch failed from api

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { ClerkProvider } from "@clerk/nextjs"
 
 import { cn } from "@/lib/utils"
 import { ModalProvider } from "@/components/providers/modal-provider"
+import { QueryProvider } from "@/components/providers/query-provider"
 import { SocketProvider } from "@/components/providers/socket-provider"
 import { ThemeProvider } from "@/components/providers/theme-provider"
 
@@ -33,7 +34,7 @@ export default function RootLayout({
           >
             <SocketProvider>
               <ModalProvider />
-              {children}
+              <QueryProvider>{children}</QueryProvider>
             </SocketProvider>
           </ThemeProvider>
         </body>
--- pages/api/socket/io.ts
+++ pages/api/socket/io.ts
@@ -12,7 +12,7 @@ export const config = {
 const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
   if (!res.socket.server.io) {
     const path = "/api/socket/io"
-    const httpServer = res.socket.server as any
+    const httpServer: NetServer = res.socket.server as any
     const io = new ServerIO(httpServer, {
       path: path,
       // @ts-ignore
@@ -20,6 +20,7 @@ const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
     })
     res.socket.server.io = io
   }
+
   res.end()
 }
 
